5a70ada5e94692c5125cffe71ba34dec737e6a09
josemicode <jmiguelojedagutierrez@gmail.com>
2024-04-26 12:10:36 +0200

    mod Musica.cpp

---
+++ Musica.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Musica.h"
+
+using namespace std;
+
+Musica::Musica() {}
+Musica::Musica(string nombre, string artista, int duracion) {
+
+}
+
+string Musica::getNombre();
+string Musica::getArtista();
+int Musica::getDuracion();
+void Musica::setNombre();
+void Musica::setArtista();
+void Musica::setDuracion();
